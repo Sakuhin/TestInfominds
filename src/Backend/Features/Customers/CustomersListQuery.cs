8d6985a57309e22a648cbb00656eb7510660fd59
Sakuhin <85579742+Sakuhin@users.noreply.github.com>
2025-06-02 18:06:24 +0000

    Aggiunta query che ritorna la lista dei Customers

---
+++ src/Backend/Features/Customers/CustomersListQuery.cs
@@ -0,0 +1,68 @@
+using MediatR;
+
+namespace Backend.Features.Customers;
+
+
+public class CustomerListQuery : IRequest<List<CustomerListQueryResponse>>
+{
+    public string? Name { get; set; }
+}
+
+public class CustomerListQueryResponse
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = "";
+    public string Address { get; set; } = "";
+    public string Email { get; set; } = "";
+    public string Phone { get; set; } = "";
+    public string Iban { get; set; } = "";
+    public CustomerListQueryResponseCustomerCategory? CustomerCategory { get; set; }
+}
+
+public class CustomerListQueryResponseCustomerCategory
+{
+    public string Code { get; set; } = "";
+    public string Description { get; set; } = "";
+}
+
+internal class CustomerListQueryHandler(BackendContext context) : IRequestHandler<CustomerListQuery, List<CustomerListQueryResponse>>
+{
+    private readonly BackendContext context = context;
+
+    public async Task<List<CustomerListQueryResponse>> Handle(CustomerListQuery request, CancellationToken cancellationToken)
+    {
+        var query = context.Customers.AsQueryable();
+        if (!string.IsNullOrEmpty(request.Name))
+            query = query.Where(q => q.Name.ToLower().Contains(request.Name.ToLower()));
+
+        var data = await query.OrderBy(q => q.Name).ToListAsync(cancellationToken);
+        var result = new List<CustomerListQueryResponse>();
+
+        foreach (var item in data)
+        {
+            var resultItem = new CustomerListQueryResponse
+            {
+                Id = item.Id,
+                Name = item.Name,
+                Address = item.Address,
+                Email = item.Email,
+                Phone = item.Phone,
+                Iban = item.Iban,
+            };
+
+            //record della tabella CustomerCategory
+            var customerCategory = await context.CustomerCategories.SingleOrDefaultAsync(q => q.Id == item.CustomerCategoryId, cancellationToken);
+            if (customerCategory is not null)
+                resultItem.CustomerCategory = new CustomerListQueryResponseCustomerCategory
+                {
+                    Code = customerCategory.Code,
+                    Description = customerCategory.Description
+                };
+
+
+            result.Add(resultItem);
+        }
+
+        return result;
+    }
+}
